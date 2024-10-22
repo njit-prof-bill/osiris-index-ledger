1c4ccba92556635470e6a9da8fdd5faac4890907
julia <alara@alarabread.fun>
2024-10-22 12:50:31 -0400

    add prettier

--- proto.ts
+++ proto.ts
@@ -1,8 +1,8 @@
-import path from 'path';
+import path from "path";
 import grpc from "@grpc/grpc-js";
 import protoLoader from "@grpc/proto-loader";
 
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,19 +13,16 @@ export function loadProtoService(
 	serviceName: string,
 ): grpc.ServiceClientConstructor {
 	const protoPath = __dirname + "/" + path;
-	const packageDefinition = protoLoader.loadSync(
-		protoPath,
-		{
-			keepCase: true,
-			longs: String,
-			enums: String,
-			defaults: true,
-			oneofs: true,
-		},
-	);
-	const packageDef = grpc.loadPackageDefinition(
-		packageDefinition,
-	)[packageName] as grpc.GrpcObject;
+	const packageDefinition = protoLoader.loadSync(protoPath, {
+		keepCase: true,
+		longs: String,
+		enums: String,
+		defaults: true,
+		oneofs: true,
+	});
+	const packageDef = grpc.loadPackageDefinition(packageDefinition)[
+		packageName
+	] as grpc.GrpcObject;
 	const proto = packageDef[serviceName] as grpc.ServiceClientConstructor;
 	return proto;
 }
