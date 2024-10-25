abbab7d387fb6f0314398844919815230ea75944
julia <alara@alarabread.fun>
2024-10-25 04:49:40 -0400

    move scripts to seperate folder

--- scripts/build_proto.ts
+++ scripts/build_proto.ts
@@ -2,13 +2,26 @@ import { rimraf } from "rimraf";
 import { glob } from "glob";
 import { exec } from "child_process";
 import { resolve } from "path";
+import { mkdirSync } from "fs";
 
 rimraf("./dist");
+
+mkdirSync("./generated");
 
 const protoFiles = (await glob("./proto/**/*.proto", { absolute: true })).join(
 	" ",
 );
 const protoPath = resolve("./proto");
-exec(
+let child = exec(
 	`npx grpc_tools_node_protoc --proto_path=${protoPath} --plugin=./node_modules/.bin/protoc-gen-ts --ts_out=./generated ${protoFiles}`,
+	(error, stdout, stderr) => {
+		if (error) {
+			console.error(error.message);
+			return;
+		}
+		if (stderr) {
+			console.error(stderr);
+		}
+		console.log(stdout);
+	},
 );
