8867761a441690ea2aba66a1eac4f3fdea8f3275
Alara Bread <2455851-AlaraBread@users.noreply.gitlab.com>
2024-10-25 19:43:19 -0400

    fix for windows

--- scripts/build_proto.ts
+++ scripts/build_proto.ts
@@ -12,16 +12,32 @@ const protoFiles = (await glob("./proto/**/*.proto", { absolute: true }))
 	.map((path) => `\"${path}\"`)
 	.join(" ");
 const protoPath = resolve("./proto");
-let child = exec(
-	`npx grpc_tools_node_protoc --proto_path=\"${protoPath}\" --plugin=./node_modules/.bin/protoc-gen-ts --ts_out=./generated ${protoFiles}`,
-	(error, stdout, stderr) => {
-		if (error) {
-			console.error(error.message);
-			return;
-		}
-		if (stderr) {
-			console.error(stderr);
-		}
-		console.log(stdout);
-	},
-);
+if (process.platform == "win32") {
+	exec(
+		`npx grpc_tools_node_protoc --proto_path=\"${protoPath}\" --plugin=".\\node_modules\\.bin\\protoc-gen-ts" --ts_out=./generated ${protoFiles}`,
+		(error, stdout, stderr) => {
+			if (error) {
+				console.error(error.message);
+				return;
+			}
+			if (stderr) {
+				console.error(stderr);
+			}
+			console.log(stdout);
+		},
+	);
+} else {
+	exec(
+		`npx grpc_tools_node_protoc --proto_path=\"${protoPath}\" --plugin=./node_modules/.bin/protoc-gen-ts --ts_out=./generated ${protoFiles}`,
+		(error, stdout, stderr) => {
+			if (error) {
+				console.error(error.message);
+				return;
+			}
+			if (stderr) {
+				console.error(stderr);
+			}
+			console.log(stdout);
+		},
+	);
+}
