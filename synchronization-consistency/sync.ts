f5f6f50c2127f981b92d5812af34bd8ef5c34ccf
Janish Suneja <jsuneja@outlook.com>
2024-11-02 05:01:39 -0400

    Deleted samson's text file

--- synchronization-consistency/sync.ts
+++ synchronization-consistency/sync.ts
@@ -1,15 +1,17 @@
 import grpc from "@grpc/grpc-js";
 import { addService, loadProtoService } from "../proto.js";
 import { hello } from "./hello/hello.js";
+import { scheduleLedgerSync } from "./schedule-ledger-sync/schedule-ledger-sync.js";
 /* Add APIs here as we complete them. */
 
 export default function addModule(server: grpc.Server) {
-	/*addService(
+	addService(
 		server,
 		loadProtoService("proto/sync/sync.proto", "sync", "IndexSynchro"),
 		{
+			ScheduleLedgerSync: scheduleLedgerSync,
 		},
-	);*/
+	);
 	addService(
 		server,
 		loadProtoService("proto/hello.proto", "helloworld", "Greeter"),
