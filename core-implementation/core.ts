781543c623c92840d7b78032180150f477bac7ad
Aidan Krenz <109118941+aidank25@users.noreply.github.com>
2024-11-02 14:17:31 -0400

    API 3 (#18)

--- core-implementation/core.ts
+++ core-implementation/core.ts
@@ -2,6 +2,7 @@ import grpc from "@grpc/grpc-js";
 import { addService, loadProtoService } from "../proto.js";
 import { hello } from "./hello/hello.js";
 import { recordTransaction } from "./record_transaction/record_transaction.js";
+import { GetTransactionDetails } from "./get_transaction_details/get_transaction_details.js";
 import { searchTransactionByUser } from "./search_transaction_by_user/search_transaction_by_user.js";
 import { getAccountBalance } from "./get_account_balance/get_account_balance.js";
 import { updateTransaction } from "./update_transaction/update_transaction.js";
@@ -16,6 +17,7 @@ export default function addModule(server: grpc.Server) {
 		loadProtoService("proto/core/core.proto", "core", "IndexLedger"),
 		{
 			RecordTransaction: recordTransaction,
+			GetTransactionDetails: GetTransactionDetails,
 			SearchTransactionsByUser: searchTransactionByUser,
 			UpdateTransaction: updateTransaction,
 			CalculateTotalLedgerValue: calculateTotalLedgerValue,
