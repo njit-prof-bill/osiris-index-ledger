cb2ffeeacd10094b4721a3922f746b71f73295f5
Janish Suneja <jsuneja@outlook.com>
2024-11-02 17:21:59 -0400

    Completed API 10!

--- dist/synchronization-consistency/sync.js
+++ dist/synchronization-consistency/sync.js
@@ -1,12 +1,12 @@
 import { addService, loadProtoService } from "../proto.js";
 import { hello } from "./hello/hello.js";
+import { scheduleLedgerSync } from "./schedule-ledger-sync/schedule-ledger-sync.js";
+import { viewLedgerSyncHistory } from "./view-ledger-sync-history/view-ledger-sync-history.js";
 /* Add APIs here as we complete them. */
 export default function addModule(server) {
-    /*addService(
-        server,
-        loadProtoService("proto/core/core.proto", "core", "IndexLedger"),
-        {
-        },
-    );*/
+    addService(server, loadProtoService("proto/sync/sync.proto", "sync", "IndexSynchro"), {
+        ScheduleLedgerSync: scheduleLedgerSync,
+        viewLedgerSyncHistory: viewLedgerSyncHistory,
+    });
     addService(server, loadProtoService("proto/hello.proto", "helloworld", "Greeter"), { SayHello: hello });
 }
--- synchronization-consistency/sync.ts
+++ synchronization-consistency/sync.ts
@@ -1,15 +1,19 @@
 import grpc from "@grpc/grpc-js";
 import { addService, loadProtoService } from "../proto.js";
 import { hello } from "./hello/hello.js";
+import { scheduleLedgerSync } from "./schedule-ledger-sync/schedule-ledger-sync.js";
+import { viewLedgerSyncHistory } from "./view-ledger-sync-history/view-ledger-sync-history.js";
 /* Add APIs here as we complete them. */
 
 export default function addModule(server: grpc.Server) {
-	/*addService(
+	addService(
 		server,
 		loadProtoService("proto/sync/sync.proto", "sync", "IndexSynchro"),
 		{
+			ScheduleLedgerSync: scheduleLedgerSync,
+			viewLedgerSyncHistory: viewLedgerSyncHistory,
 		},
-	);*/
+	);
 	addService(
 		server,
 		loadProtoService("proto/hello.proto", "helloworld", "Greeter"),
