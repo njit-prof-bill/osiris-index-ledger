baf9283e26bf40dc8078d82cbb5e7341baaba5b7
Nathaniel Kapleau <nlk9@njit.edu>
2024-11-03 23:01:31 -0500

    Testing API 7

--- synchronization-consistency/sync.ts
+++ synchronization-consistency/sync.ts
@@ -2,7 +2,7 @@ import grpc from "@grpc/grpc-js";
 import { addService, loadProtoService } from "../proto.js";
 import { hello } from "./hello/hello.js";
 import { ForceLedgerReconciliation } from "./force_ledger_reconciliation/force_ledger_reconciliation.js"
-import { BroadcastTransaction } from "./broadcast_transaction_to_nodes/broadcast_transaction_to_nodes.js";
+//import { BroadcastTransaction } from "./broadcast_transaction_to_nodes/broadcast_transaction_to_nodes.js";
 /* Add APIs here as we complete them. */
 
 export default function addModule(server: grpc.Server) {
