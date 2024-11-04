b19b17940800f90d2bfd14dd12cef4390158bdbf
Irving <irvingcastillo15834@gmail.com>
2024-11-04 04:53:34 -0500

    Initial Commit

--- synchronization-consistency/sync.ts
+++ synchronization-consistency/sync.ts
@@ -1,15 +1,18 @@
 import grpc from "@grpc/grpc-js";
 import { addService, loadProtoService } from "../proto.js";
 import { hello } from "./hello/hello.js";
-/* Add APIs here as we complete them. */
+import { syncLedger } from "./sync_ledger/sync_ledger.js";
+import { fetchMissingTransactions } from "./fetch_missing_transactions_command/fetch.js";
 
 export default function addModule(server: grpc.Server) {
-	/*addService(
+	addService(
 		server,
 		loadProtoService("proto/sync/sync.proto", "sync", "IndexSynchro"),
 		{
+			SyncLedger: syncLedger,
+			FetchMissingTransactions: fetchMissingTransactions
 		},
-	);*/
+	);
 	addService(
 		server,
 		loadProtoService("proto/hello.proto", "helloworld", "Greeter"),
